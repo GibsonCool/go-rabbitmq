ea08eeb391edfa2515764db03f6d1d6bb4b80af5
DoubleX <double@tg10010.com>
2019-11-13 10:48:40 +0700

    add：注释说明

--- 02_workMode/workPublish.go
+++ 02_workMode/workPublish.go
@@ -11,7 +11,7 @@ import (
 		生产速度大于消费速度，一个生产端，多个消费端。和simple模式一样，一个消息只能被一个消费端消费
 
 		workReceiver1  workReceiver2 workReceiver2 都是一模一样的代码，起到的作用其实就是负载均衡的效果
-		从运行的结果可以看出这里的负载均衡策略基本是轮询三个消费端接受
+		从运行的结果可以看出这里的负载均衡策略基本是轮询三个消费端接收
 */
 func main() {
 	rabbitMQSimple := _0_RabbitMQ.NewRabbitMQSimple(_0_RabbitMQ.SimpleQueueName)
--- 04_routingMode/routingPublish.go
+++ 04_routingMode/routingPublish.go
@@ -7,13 +7,19 @@ import (
 	"time"
 )
 
+/*
+	路由模式：
+		一个生产端，多个消费端。路由模式下，生产端可以根据路由 Key 指定那种路由 key 消费端接收消费消息
+*/
 func main() {
 	rabbitMQRoutingOne := _0_RabbitMQ.NewRabbitMQRouting(_0_RabbitMQ.RoutingExchangeName, _0_RabbitMQ.RoutingKey1)
 	rabbitMQRoutingTwo := _0_RabbitMQ.NewRabbitMQRouting(_0_RabbitMQ.RoutingExchangeName, _0_RabbitMQ.RoutingKey2)
 	for i := 0; i <= 10; i++ {
 		log.Printf("正在发送第 %d 条消息~~~", i)
 		rabbitMQRoutingOne.PublishRouting(fmt.Sprintf("Routing key:%s 路由模式消息 index:%d", rabbitMQRoutingOne.Key, i))
-		rabbitMQRoutingTwo.PublishRouting(fmt.Sprintf("Routing key:%s 路由模式消息 index:%d", rabbitMQRoutingTwo.Key, i))
+		if i%2 == 0 {
+			rabbitMQRoutingTwo.PublishRouting(fmt.Sprintf("Routing key:%s 路由模式消息 index:%d", rabbitMQRoutingTwo.Key, i))
+		}
 		time.Sleep(1 * time.Second)
 	}
 
